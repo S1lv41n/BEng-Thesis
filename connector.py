a1f60673d4bf52306f2baa4c4893555e882face4
S1lv41n <jakub.szczeniak54@gmail.com>
2020-11-27 11:27:15 +0100

    User can nowe choose what action will be executed

--- connector.py
+++ connector.py
@@ -3,19 +3,39 @@ from mysql.connector import Error
 
 
 connection = mysql.connector.connect(host='localhost', database='inżynierka', user='s1lv41n', password='S1lv41nftw!', auth_plugin='mysql_native_password')
-
-db_info = connection.get_server_info()
-print("Connected to MySQL Server version ", db_info)
 cursor = connection.cursor()
-cursor.execute("select database();")
-record = cursor.fetchone()
-print("Połączyłeś się z bazą: ", record)
 
-mySql_insert_record = """INSERT INTO produkty (prod_id, nazwa, ilosc, wlasciwosci, rodzaj)                          VALUES 
-                           (1, 'Produkt A', 3, 'przykładowy Produkt A', 1)"""
+#print("Co chcesz zrobić? \n 1. Dodaj rekord \n 2. Usuń rekord")
+action = input("Co chcesz zrobić? \n 1. Dodaj rekord \n 2. Usuń rekord\n")
 
-cursor = connection.cursor()
-cursor.execute(mySql_insert_record)
-connection.commit()
-print(cursor.rowcount, "Rekord został pomyślnie dodany do tabeli Produkty")
-cursor.close()
+if action == "1":
+    db_info = connection.get_server_info()
+    print("Connected to MySQL Server version ", db_info)
+
+    cursor.execute("select database();")
+    record = cursor.fetchone()
+    print("Połączyłeś się z bazą: ", record)
+
+    mySql_insert_record = """INSERT INTO produkty (prod_id, nazwa, ilosc, wlasciwosci, rodzaj) VALUES
+                            (1, 'Produkt A', 3, 'przykładowy Produkt A', 1)"""
+
+    cursor.execute(mySql_insert_record)
+    connection.commit()
+    print(cursor.rowcount, "Rekord został pomyślnie dodany do tabeli Produkty")
+    cursor.close()
+
+if action == "2":
+    print("Wyświetlam obecne wartości przed ich usunięciem: ")
+    sql_select_record = """SELECT * FROM produkty WHERE prod_id = 1"""
+    cursor.execute(sql_select_record)
+    record = cursor.fetchone()
+    print(record)
+
+    sql_delete_record = """DELETE from produkty WHERE prod_id = 1"""
+    cursor.execute(sql_delete_record)
+    connection.commit()
+
+    cursor.execute(sql_select_record)
+    records = cursor.fetchall()
+    if len(records) == 0:
+        print("\nRekord został pomyślnie usunięty")
