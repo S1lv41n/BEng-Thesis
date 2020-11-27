8004ef443ce9bb682a7f3c635fa94d5f88b6801a
S1lv41n <jakub.szczeniak54@gmail.com>
2020-11-27 10:56:15 +0100

    Implemented insertion just for testing

--- connector.py
+++ connector.py
@@ -4,9 +4,18 @@ from mysql.connector import Error
 
 connection = mysql.connector.connect(host='localhost', database='inżynierka', user='s1lv41n', password='S1lv41nftw!', auth_plugin='mysql_native_password')
 
-db_Info = connection.get_server_info()
-print("Connected to MySQL Server version ", db_Info)
+db_info = connection.get_server_info()
+print("Connected to MySQL Server version ", db_info)
 cursor = connection.cursor()
 cursor.execute("select database();")
 record = cursor.fetchone()
-print("You're connected to database: ", record)
+print("Połączyłeś się z bazą: ", record)
+
+mySql_insert_record = """INSERT INTO produkty (prod_id, nazwa, ilosc, wlasciwosci, rodzaj)                          VALUES 
+                           (1, 'Produkt A', 3, 'przykładowy Produkt A', 1)"""
+
+cursor = connection.cursor()
+cursor.execute(mySql_insert_record)
+connection.commit()
+print(cursor.rowcount, "Rekord został pomyślnie dodany do tabeli Produkty")
+cursor.close()
