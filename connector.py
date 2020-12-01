042fb90b9467b59c463c72679b289e6e61f441fc
S1lv41n <jakub.szczeniak54@gmail.com>
2020-12-01 12:52:28 +0100

    New code changes

--- connector.py
+++ connector.py
@@ -1,24 +1,19 @@
 import mysql.connector
-from mysql.connector import Error
-
 
 connection = mysql.connector.connect(host='localhost', database='inżynierka', user='s1lv41n', password='S1lv41nftw!', auth_plugin='mysql_native_password')
 cursor = connection.cursor()
 
-#print("Co chcesz zrobić? \n 1. Dodaj rekord \n 2. Usuń rekord")
+db_info = connection.get_server_info()
+print("Connected to MySQL Server version ", db_info)
+cursor.execute("select database();")
+record = cursor.fetchone()
+print("Połączyłeś się z bazą:", record)
+
 action = input("Co chcesz zrobić? \n 1. Dodaj rekord \n 2. Usuń rekord\n")
 
 if action == "1":
-    db_info = connection.get_server_info()
-    print("Connected to MySQL Server version ", db_info)
-
-    cursor.execute("select database();")
-    record = cursor.fetchone()
-    print("Połączyłeś się z bazą: ", record)
-
-    mySql_insert_record = """INSERT INTO produkty (prod_id, nazwa, ilosc, wlasciwosci, rodzaj) VALUES
-                            (1, 'Produkt A', 3, 'przykładowy Produkt A', 1)"""
-
+    values = input("Podaj wartości rekordu: ")
+    mySql_insert_record = "INSERT INTO produkty (prod_id, nazwa, ilosc, wlasciwosci, rodzaj) VALUES (1, 'Produkt A', 3, 'przykładowy Produkt A', 1)"
     cursor.execute(mySql_insert_record)
     connection.commit()
     print(cursor.rowcount, "Rekord został pomyślnie dodany do tabeli Produkty")
@@ -38,4 +33,4 @@ if action == "2":
     cursor.execute(sql_select_record)
     records = cursor.fetchall()
     if len(records) == 0:
-        print("\nRekord został pomyślnie usunięty")
+        print("Rekord został pomyślnie usunięty")
