f0c2186b3c08106c09f3d467a9d799855e102be5
S1lv41n <jakub.szczeniak54@gmail.com>
2020-12-13 11:18:48 +0100

    Updates to connector.py
    User can now specify record values when adding it to the database.
    User can also choose which item is going to be deleted by prod_id and cancel that action

--- connector.py
+++ connector.py
@@ -1,6 +1,10 @@
 import mysql.connector
 
-connection = mysql.connector.connect(host='localhost', database='inżynierka', user='s1lv41n', password='S1lv41nftw!', auth_plugin='mysql_native_password')
+connection = mysql.connector.connect(host='localhost',
+                                    database='inżynierka', 
+                                    user='s1lv41n', 
+                                    password='S1lv41nftw!', 
+                                    auth_plugin='mysql_native_password')
 cursor = connection.cursor()
 
 db_info = connection.get_server_info()
@@ -12,25 +16,34 @@ print("Połączyłeś się z bazą:", record)
 action = input("Co chcesz zrobić? \n 1. Dodaj rekord \n 2. Usuń rekord\n")
 
 if action == "1":
-    values = input("Podaj wartości rekordu: ")
-    mySql_insert_record = "INSERT INTO produkty (prod_id, nazwa, ilosc, wlasciwosci, rodzaj) VALUES (1, 'Produkt A', 3, 'przykładowy Produkt A', 1)"
-    cursor.execute(mySql_insert_record)
+    #TODO Add new columns in 'produkty' table
+    #TODO prod_id by auto_increment
+    prod_id = input("prod_id: ")
+    nazwa = input ("nazwa: ")
+    ilosc = input ("ilosc: ")
+    mySql_insert_record = """INSERT INTO produkty (prod_id, nazwa, ilosc) VALUES (%s, %s, %s)"""
+    cursor.execute(mySql_insert_record, (prod_id, nazwa, ilosc), multi=True)
     connection.commit()
-    print(cursor.rowcount, "Rekord został pomyślnie dodany do tabeli Produkty")
+    print("Rekord został pomyślnie dodany do tabeli Produkty")
     cursor.close()
 
 if action == "2":
+    prod_id = (input("prod_id: "), )
     print("Wyświetlam obecne wartości przed ich usunięciem: ")
-    sql_select_record = """SELECT * FROM produkty WHERE prod_id = 1"""
-    cursor.execute(sql_select_record)
-    record = cursor.fetchone()
+    sql_select_record = """SELECT * FROM produkty WHERE prod_id = %s"""
+    cursor.execute(sql_select_record, prod_id)
+    record = cursor.fetchall()
     print(record)
 
-    sql_delete_record = """DELETE from produkty WHERE prod_id = 1"""
-    cursor.execute(sql_delete_record)
-    connection.commit()
-
-    cursor.execute(sql_select_record)
-    records = cursor.fetchall()
-    if len(records) == 0:
+    confirmation = input("Czy napewno chcesz usunąć ten rekord? y/n \n")
+    
+    if confirmation == "y":
+        sql_delete_record = """DELETE from produkty WHERE prod_id = %s"""
+        cursor.execute(sql_delete_record, prod_id)
+        connection.commit()
+        cursor.close()
         print("Rekord został pomyślnie usunięty")
+        
+    if confirmation == "n":
+        print ("Rekord nie został usunięty")
+        cursor.close()
